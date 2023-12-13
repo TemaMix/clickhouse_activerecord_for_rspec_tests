80ec5ababf83e850c066bbb0ff3272d3831d2f21
TemaMix <tema.place@gmail.com>
2023-12-13 18:31:18 +0300

    Initial commit

---
+++ lib/clickhouse_activerecord_for_rspec_tests.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "clickhouse_activerecord_for_rspec_tests/version"
+
+module ClickhouseActiverecordForRspecTests
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/clickhouse_activerecord_for_rspec_tests_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe ClickhouseActiverecordForRspecTests do
+  it "has a version number" do
+    expect(ClickhouseActiverecordForRspecTests::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
